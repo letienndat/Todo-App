ee31e3aed751069e6be67da5f2306645b7ca3340
letienndat <attn110601@gmail.com>
2024-12-21 10:17:43 +0700

    Initial Commit

---
+++ Todo-App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo-App
+//
+//  Created by Lê Tiến Đạt on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
